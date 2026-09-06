7e6720cc9add654e019ae8d9f047b576a93e9daf
Rory Norman <85381484+Renorman@users.noreply.github.com>
2026-09-05 22:00:21 -0500

    feat: Added in synopsis + tasks for website page

--- src/components/sections/PortfolioPages/ThisWebsite.jsx
+++ src/components/sections/PortfolioPages/ThisWebsite.jsx
@@ -8,8 +8,15 @@ const ThisWebsiteData = {
   role: <span><em>Solo</em> - Designer, Modeller, and Programmer</span>,
   icons: [{ src: "./assets/AboutIcons/Technologies/Figma.png", alt: 'Figma' }, { src: "./assets/AboutIcons/Technologies/Blender.png", alt: 'Blender' }, { src: "./assets/AboutIcons/Technologies/Affinity.png", alt: 'Affinity' }, { src: "./assets/AboutIcons/Technologies/React.png", alt: 'React' }, { src: "./assets/AboutIcons/Technologies/Tailwind.png", alt: 'Tailwind' }, { src: "./assets/AboutIcons/Technologies/ThreeJS.png", alt: 'ThreeJS' }, { src: "./assets/AboutIcons/Languages/CSS.png", alt: 'CSS' }, { src: "./assets/AboutIcons/Languages/HTML.png", alt: 'HTML' }],
   splashSrc: "./assets/gallery/this_website/this_website_splash.png",
-  synopsis: <span></span>,
-  tasks: <span></span>,
+  synopsis: <span>
+    My portfolio website serves as a representation of everything I’m capable of, as well as quite literally, displaying what I’ve historically been capable of. 
+
+    <br></br>
+    <br></br>
+  
+    I designed, modelled, developed, and deployed this entire website start-to-finish from scratch. I taught and re-taught myself React, Tailwind CSS, JavaScript, HTML, CSS, and Three.js just to make it feel a bit more custom to my interests. And, I’m very proud of the final result. 
+  </span>,
+  tasks: <span>Lead developer, artist, and designer</span>,
   renderImages: ThisWebsiteRenders,
   progressImages: ThisWebsiteProgress,
   challenges: [
